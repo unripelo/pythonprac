1ef13c865ffc9fddf4ab9024c012718adbbb788f
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-01-01 21:24:59 +0800

    adding strings

---
+++ string-1/make-abba.py
@@ -0,0 +1,7 @@
+def make_abba(a, b):
+  return a + b + b + a
+
+print(make_abba('Hi', 'Bye'))
+print(make_abba('Yo', 'What'))
+print(make_abba('Ba', 'by'))
+print(make_abba('Ha', 'ns'))
--- warmpup-1/sum_double.py
+++ warmpup-1/sum_double.py
@@ -1,3 +1,5 @@
+# Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
 def sum_double(a, b):
   sum = a + b
   if a == b:
