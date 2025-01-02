c4dfc5eea35cce7a65d101c0926028dbdc41879f
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-01-02 14:58:46 +0800

    adding operator

---
+++ snake/operator-list.py
@@ -0,0 +1,12 @@
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+print(x > 3 and x < 10)
+print(x > 3 or x < 10)
+print(not( x > 3 and x < 10))
