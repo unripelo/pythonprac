c62618c5a7981f40859d30c214fdda4ae5dc147a
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-01-11 22:24:01 +0800

    adding string

---
+++ warmup-2/string-bits.py
@@ -0,0 +1,10 @@
+def string_bits(str):
+  result = ""
+  for i in range(len(str)):
+    if i % 2 == 0:
+      result = result + str[i]
+  return result
+  
+print(string_bits('Hello'))
+print(string_bits('Hi')) 
+print(string_bits('Heeololeo'))
